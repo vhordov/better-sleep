77ade81cbad500f0fefe225c3750aed4df69ff9f
Vadym Hordov <vadymhordov@gmail.com>
2020-08-27 19:59:35 -0400

    Made more design changes

--- Better Sleep/ContentView.swift
+++ Better Sleep/ContentView.swift
@@ -52,31 +52,69 @@ struct ContentView: View {
 //    }
     
     var body: some View {
-        VStack {
-                VStack {
-                    Image("Sun")
-                    Text("18:56")
-                        .font(.custom("chalkboard", size: 70))
-                        .foregroundColor(Color.orange)
+        ScrollView {
+            VStack(spacing: 0) {
+                ZStack {
+                    Image("Stars")
+                    .scaledToFill()
+                    VStack {
+                        Image("Sun")
+                        Text("18:56")
+                            .font(.custom("chalkboard", size: 70))
+                            .foregroundColor(Color.orange)
+                    }
                 }
-            .background(Image("Stars"))
-            .frame(width: 414, height: 681)
-            .edgesIgnoringSafeArea(.all)
-            VStack {
-               Text("When do you want to wake up?")
-                .frame(width: 414, height: 29)
-                .font(.custom("sf-compact", size: 24))
-                .background(Color.white)
-                .foregroundColor(Color.orange)
+                ZStack {
+                    LinearGradient(gradient: Gradient(colors: [
+                        Color(red: 240.0 / 255.0, green: 212.0 / 255.0, blue: 146.0 / 255.0),
+                        Color(red: 229.0 / 255.0, green: 155.0 / 255.0, blue: 112.0 / 255.0)]),
+                        startPoint: .bottomLeading,
+                        endPoint: .topTrailing
+                    )
+                    VStack {
+                       Text("When do you want to wake up?")
+                        .font(.custom("sf-compact", size: 24))
+                        .foregroundColor(Color.white)
+                        DatePicker("Please enter a date", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                            .labelsHidden()
+                            .background(Color.white
+                                .frame(width: 414, height: 215))
+                        Text("Desired amount of sleep")
+                            .font(.custom("sf-compact", size: 24))
+                            .foregroundColor(Color.white)
+                        Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
+                        Text("\(sleepAmount, specifier: "%.2f") hours")
+                        }
+                        .padding(25.0)
+                        .frame(width: 257.0, height: 40.0)
+                    }
+                    .padding(.top, 25.0)
+                    
+                    }
             }
-            DatePicker("Please enter a date", selection: $wakeUp, displayedComponents: .hourAndMinute)
-            .labelsHidden()
-            Text("Desired amount of sleep")
-            .frame(width: 414, height: 29)
-            .font(.custom("sf-compact", size: 24))
-            .background(Color.white)
-            .foregroundColor(Color.orange)
+            ZStack {
+                LinearGradient(gradient: Gradient(colors: [
+                    Color(red: 240.0 / 255.0, green: 212.0 / 255.0, blue: 146.0 / 255.0),
+                    Color(red: 229.0 / 255.0, green: 155.0 / 255.0, blue: 112.0 / 255.0)]),
+                    startPoint: .bottomLeading,
+                    endPoint: .topTrailing
+                )
+                VStack {
+                Text("Daily coffee intake")
+                    .font(.custom("sf-compact", size: 24))
+                    .foregroundColor(Color.white)
+                    Stepper(value: $coffeeAmount, in: 1...20, step: 1) {
+                    if coffeeAmount == 1 {
+                    Text("1 cup")
+                    } else {
+                    Text("\(coffeeAmount) cups")
+                        .frame(width: 257.0, height: 40.0)
+                        }
+                    }
+                }
+            }
         }
+        .edgesIgnoringSafeArea(.all)
     }
     
     func calculateBedTime() {
